2f408765d44695fff048548c0f2a627367d11ac0
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-09-27 19:39:59 -0300

    feat: interfaces, model and migrations of teams, matches, users tables

---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,62 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+import Team from './TeamModel';
+
+class Match extends Model<InferAttributes<Match>,
+InferCreationAttributes<Match>> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Match.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'Match',
+  timestamps: false,
+  underscored: true,
+  tableName: 'matches',
+});
+
+Match.belongsTo(Team, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+Match.belongsTo(Team, { foreignKey: 'awayTeamId', as: 'awayTeam' });
+Team.hasMany(Match, { foreignKey: 'homeTeamId', as: 'homeMatch' });
+Team.hasMany(Match, { foreignKey: 'awayTeamId', as: 'awayMatch' });
+
+export default Match;
---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,35 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class Team extends Model<InferAttributes<Team>,
+InferCreationAttributes<Team>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'Team',
+  timestamps: false,
+  underscored: true,
+  tableName: 'teams',
+});
+
+export default Team;
