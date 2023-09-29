c5eeffdc379faf9e4ab3ac5660e02518c09d3711
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-09-29 19:01:15 -0300

    fix

--- app/backend/src/services/user.services.ts
+++ app/backend/src/services/user.services.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import UserModel from '../models/UserModel';
 import { ILogin } from '../Interfaces/users/IUser';
 import { IUserModel } from '../Interfaces/users/IUserModel';
